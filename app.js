ac04900d0435cb95b5b4b3b51e54a451a50a7a06
Alexander <alex_1128@hotmail.es>
2019-02-24 18:08:54 -0500

    yarg

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', {
+        base: {
+
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+
+            alias: 'l',
+            default: 10
+
+        }
+    })
+    .help()
+    .argv;
 const { nuevaTabla } = require('./multiplicar/multiplicar');
 
 // let base = 'lol';
 
 // console.log(process.argv);
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split("=")[1];
+let argv2 = process.argv;
 
-nuevaTabla(base).then(archivo => console.log(`Archivo creado: ${ archivo }`))
-    .catch(e => console.log(e));
+console.log(argv);
+// let parametro = argv[2];
+// let base = parametro.split("=")[1];
+
+// nuevaTabla(base).then(archivo => console.log(`Archivo creado: ${ archivo }`))
+//     .catch(e => console.log(e));
